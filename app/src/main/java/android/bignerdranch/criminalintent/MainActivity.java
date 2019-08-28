5b34b6bef4c1444f66cc5a26ec8192272f5d7a08
usc-aja028 <48264426+usc-aja028@users.noreply.github.com>
2019-08-28 22:32:53 +1000

    Initial commit

---
+++ app/src/main/java/android/bignerdranch/criminalintent/MainActivity.java
@@ -0,0 +1,15 @@
+package android.bignerdranch.criminalintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+public class MainActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeFragment();
+    }
+}
